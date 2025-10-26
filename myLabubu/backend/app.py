c8bb9c9fd6dfbac514937cce742816b279cd309d
Vianna Huynh <viannakhuynh@gmail.com>
2025-10-26 03:04:03 -0400

    sfvskjdhfyugsdfugdgjhsdf

--- myLabubu/backend/app.py
+++ myLabubu/backend/app.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://www.awwsheet.ink",
+        "https://canes-chicken.vercel.app.ink",
     ],
     allow_credentials=True,
     allow_methods=["*"],
