44e7f0d9f2dccfa9bd677f791e45137b80469b7e
Vianna Huynh <viannakhuynh@gmail.com>
2025-10-26 02:54:10 -0400

    Update orgialifaddfsddsdgsfg

--- myLabubu/backend/app.py
+++ myLabubu/backend/app.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://www.awwsheet.ink",
     ],
     allow_credentials=True,
     allow_methods=["*"],
